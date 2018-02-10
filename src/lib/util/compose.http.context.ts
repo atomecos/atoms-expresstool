b501daa85b74ec5865d9bebd09f92a4b37742314
Panit Tuangsuwan <tpanitte@hotmail.com>
2018-02-10 12:04:44 +0700

    CHANGE: composeHttpContext()

--- src/lib/util/compose.http.context.ts
+++ src/lib/util/compose.http.context.ts
@@ -1,14 +1,126 @@
+import * as URL from "url";
 import { Request, Response } from "express";
 import { HttpContext, HttpContextStore } from "atoms-httpcore";
 import { IProcessContext } from "atomservicescore";
 
 export const composeHttpContext = (request: Request, response: Response, processContext?: IProcessContext): HttpContext<Request, Response> => {
-  return {
-    process: processContext,
-    store: new HttpContextStore(),
-    method: request.method,
-    path: request.path,
-    request,
-    response,
-  };
+  return new HttpContextWrapper(request, response, processContext);
 };
+
+class HttpContextWrapper implements HttpContext<Request, Response> {
+  process: IProcessContext;
+  store: HttpContextStore;
+  request: Request;
+  response: Response;
+
+  get header() {
+    return this.headers;
+  }
+
+  set header(val: any) {
+    this.headers = val;
+  }
+
+  get headers() {
+    return this.request.headers;
+  }
+
+  set headers(val: any) {
+    this.request.headers = val;
+  }
+
+  get method() {
+    return this.request.method;
+  }
+
+  set method(val: string) {
+    this.request.method = val;
+  }
+
+  get href() {
+    return `${this.origin}${this.url}`;
+  }
+
+  get origin() {
+    return `${this.protocol}://${this.host}`;
+  }
+
+  get host() {
+    return this.request.host;
+  }
+
+  get hostname() {
+    return this.request.hostname;
+  }
+
+  get protocol() {
+    return this.request.protocol;
+  }
+
+  get originalUrl() {
+    return this.request.originalUrl;
+  }
+
+  get url() {
+    return this.request.url;
+  }
+
+  set url(val: any) {
+    this.request.url = val;
+  }
+
+  get path() {
+    return this.request.path;
+  }
+
+  get querystring() {
+    return URL.parse(this.url).query;
+  }
+
+  get query() {
+    return this.request.query;
+  }
+
+  set body(val: any) {
+    this.response.send(val);
+  }
+
+  set status(val: any) {
+    this.response.sendStatus(val);
+  }
+
+  set message(val: any) {
+    this.response.send(val);
+  }
+
+  set length(val: string) {
+    this.set("Content-Length", val);
+  }
+
+  set type(val: string) {
+    this.response.type(val);
+  }
+
+  constructor(request: Request, response: Response, processContext: IProcessContext) {
+    this.process = processContext;
+    this.request = request;
+    this.response = response;
+    this.store = new HttpContextStore();
+  }
+
+  get(field: string) {
+    return this.request.get(field);
+  }
+
+  set(field: string | any, value?: string) {
+    if (typeof field === "string") {
+      this.response.set(field, value);
+    } else {
+      this.response.set(field);
+    }
+  }
+
+  append(field: string, value: string) {
+    this.response.append(field, value);
+  }
+}
